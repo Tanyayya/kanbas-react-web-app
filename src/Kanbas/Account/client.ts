b87fbce6cafe44ddb766db1801caf88d96e5f2b8
tanyashukla <shukla.tan@northeastern.edu>
2024-12-03 19:15:53 -0500

    Assignment 6 first commit

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const USERS_API = `https://kanbas-node-server-app-5z6w.onrender.com/api/users`;
+export const USERS_API = `http://localhost:4000/api/users`;
 const axiosWithCredentials = axios.create({ withCredentials: true });
-export const ENROLLMENTS_API = `https://kanbas-node-server-app-5z6w.onrender.com/api/enrollments`;
+export const ENROLLMENTS_API = `http://localhost:4000/api/enrollments`;
 
 
 export const signin = async (credentials: any) => {
@@ -21,10 +21,7 @@ export const signin = async (credentials: any) => {
     const response = await axiosWithCredentials.post(`${USERS_API}/signout`);
     return response.data;
   };
-  export const updateUser = async (user: any) => {
-    const response = await axiosWithCredentials.put(`${USERS_API}/${user._id}`, user);
-    return response.data;
-  };
+
   export const findMyCourses = async () => {
     const { data } = await axiosWithCredentials.get(`${USERS_API}/current/courses`);
     
@@ -34,14 +31,19 @@ export const signin = async (credentials: any) => {
     const {data} = await axiosWithCredentials.get(`${USERS_API}/${userId}/enrolled-courses`);
     return data;
   }
-
+  export const findCoursesForUser = async (userId: string) => {
+    const response = await axiosWithCredentials.get(`${USERS_API}/${userId}/courses`);
+    return response.data;
+  };
+  
+  
   export const createCourse = async (course: any) => {
     const { data } = await axiosWithCredentials.post(`${USERS_API}/current/courses`, course);
     return data;
   };
   
   export const findAllCourses=async()=>{
-    const {data}=await axiosWithCredentials.get(`https://kanbas-node-server-app-5z6w.onrender.com/api/courses`);
+    const {data}=await axiosWithCredentials.get(`http://localhost:4000/api/courses`);
     return data;
   }
   export const addEnrollment = async (enrollment: any) => {
@@ -67,6 +69,50 @@ export const unenrollUser = async (unenrollment: { userId: string; courseId: str
   const response = await axios.delete(ENROLLMENTS_API, { data: unenrollment });
   return response.data;
 };
+
+export const findAllUsers = async () => {
+  const response = await axiosWithCredentials.get(USERS_API);
+  
+  return response.data;
+};
+export const findUsersByRole = async (role: string) => {
+  const response = await
+    axios.get(`${USERS_API}?role=${role}`);
+  return response.data;
+};
+export const findUsersByPartialName = async (name: string) => {
+  const response = await axios.get(`${USERS_API}?name=${name}`);
+  return response.data;
+};
+export const findUserById = async (id: string) => {
+  const response = await axios.get(`${USERS_API}/${id}`);
+  return response.data;
+};
+export const deleteUser = async (userId: string) => {
+  const response = await axios.delete( `${USERS_API}/${userId}` );
+  return response.data;
+};
+
+export const updateUser = async (user: any) => {
+  const response = await axiosWithCredentials.put(`${USERS_API}/${user._id}`, user);
+  return response.data;
+};
+export const createUser = async (user: any) => {
+  const response = await axios.post(`${USERS_API}`, user);
+  return response.data;
+};
+export const enrollIntoCourse = async (userId: string, courseId: string) => {
+  const response = await axios.post(`${USERS_API}/${userId}/courses/${courseId}`);
+  return response.data;
+ };
+ export const unenrollFromCourse = async (userId: string, courseId: string) => {
+  const response = await axios.delete(`${USERS_API}/${userId}/courses/${courseId}`);
+  return response.data;
+ };
+ 
+
+
+
   
     
   
--- src/Kanbas/Courses/Assignments/client.ts
+++ src/Kanbas/Courses/Assignments/client.ts
@@ -1,12 +1,16 @@
 import axios from "axios";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
-const ASSIGNMENT_API = `https://kanbas-node-server-app-5z6w.onrender.com/api/assignments`;
+const axiosWithCredentials = axios.create({ withCredentials: true });
+
+const ASSIGNMENT_API = `http://localhost:4000/api/assignments`;
 export const deleteAssignment = async (assignmentId: string) => {
- const response = await axios.delete(`${ASSIGNMENT_API}/${assignmentId}`);
+ const response = await axiosWithCredentials.delete(`${ASSIGNMENT_API}/${assignmentId}`);
  return response.data;
 };
 export const updateAssignment = async (assignment: any) => {
-    const { data } = await axios.put(`${ASSIGNMENT_API}/${assignment._id}`, assignment);
+    const { data } = await axiosWithCredentials.put(`${ASSIGNMENT_API}/${assignment._id}`, assignment);
     return data;
   };
+ 
+   
   
