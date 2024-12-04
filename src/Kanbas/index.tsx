7f5d082e16467b0500c7197995a0f182d9742023
Tanya Shukla <68274356+Tanyayya@users.noreply.github.com>
2024-12-03 23:11:12 -0500

    Update index.tsx

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -58,7 +58,7 @@ export default function Kanbas() {
 
   const deleteCourse = async (courseId: string) => {
     const status = await courseClient.deleteCourse(courseId);
-    setAllCourses(courses.filter((course) => course._id !== courseId));
+    setAllCourses(allCourses.filter((course) => course._id !== courseId));
     setEnrolledCourses((prevEnrolled) => prevEnrolled.filter((c) => c._id !== courseId));
 
   };
@@ -67,7 +67,7 @@ export default function Kanbas() {
     await courseClient.updateCourse(course);
     const updatedEnrolledCourses = await userClient.findMyCourses();
       setEnrolledCourses(updatedEnrolledCourses);
-    setAllCourses(courses.map((c) => {
+    setAllCourses(allCourses.map((c) => {
         if (c._id === course._id) { return course; }
         else { return c; }
     })
