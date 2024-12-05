4fbb126139df77a5a25c8aaccece8e8e9860a81f
tanyashukla <shukla.tan@northeastern.edu>
2024-12-05 15:20:56 -0500

    server link changed

--- src/Labs/lab5/WorkingWithArrays.tsx
+++ src/Labs/lab5/WorkingWithArrays.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 export default function WorkingWithArrays() {
-  const API = `https://kanbas-node-server-app-1-r717.onrender.com/lab5/todos`;
+  const API = `http://localhost:4000/lab5/todos`;
   const [todo, setTodo] = useState({id: "1",title: "NodeJS Assignment",
     description: "Create a NodeJS server with ExpressJS",
     due: "2021-09-09",
