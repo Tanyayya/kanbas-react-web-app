0b2baadc5c60e822d4913e1331174679daa655b6
Tanya Shukla <68274356+Tanyayya@users.noreply.github.com>
2024-12-12 18:54:49 -0500

    Update client.ts

--- src/Kanbas/Courses/Modules/client.ts
+++ src/Kanbas/Courses/Modules/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
-const MODULES_API = `http://localhost:4000/api/modules`;
+const MODULES_API = `https://kanbas-node-server-app-5z6w.onrender.com/api/modules`;
 export const deleteModule = async (moduleId: string) => {
  const response = await axios.delete(`${MODULES_API}/${moduleId}`);
  return response.data;
