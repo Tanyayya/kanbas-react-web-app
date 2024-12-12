8bae3a0a000fd0336ee30388e759c136deb14f3c
Tanya Shukla <68274356+Tanyayya@users.noreply.github.com>
2024-12-12 18:46:56 -0500

    Update client.ts

--- src/Kanbas/Courses/Assignments/client.ts
+++ src/Kanbas/Courses/Assignments/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
-const ASSIGNMENT_API = `https://kanbas-node-server-app-5z6w.onrender.com/api/assignments`;
+const ASSIGNMENT_API = `http://localhost:4000/api/assignments`;
 export const deleteAssignment = async (assignmentId: string) => {
  const response = await axios.delete(`${ASSIGNMENT_API}/${assignmentId}`);
  return response.data;
