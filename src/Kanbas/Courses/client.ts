a3f4b2b8c3782ee811c959e64aa7c6b620e40aee
Tanya Shukla <68274356+Tanyayya@users.noreply.github.com>
2024-12-12 18:54:30 -0500

    Update client.ts

--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const COURSES_API = `http://localhost:4000/api/courses`;
+const COURSES_API = `https://kanbas-node-server-app-5z6w.onrender.com/api/courses`;
 export const fetchAllCourses = async () => {
   const { data } = await axios.get(COURSES_API);
   
